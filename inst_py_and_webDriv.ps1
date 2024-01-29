2344c1f7b85b001555951ba5b2f219a46fec55a7
Kumakura <LVF00047@astellas.com>
2024-01-29 18:16:32 +0900

    modified inst_py_and_webDriv.ps1

--- inst_py_and_webDriv.ps1
+++ inst_py_and_webDriv.ps1
@@ -1,15 +1,36 @@
-# PythonのインストーラーのURL
-$pythonUrl = "https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe" 
-# Pythonのインストーラーのパス
-$installerPath = "$env:TEMP\python_installer.exe"
-Invoke-WebRequest -Uri $pythonUrl -OutFile $installerPath
-Start-Process $installerPath -ArgumentList '/quiet InstallAllUsers=0 PrependPath=1' -Wait
-Remove-Item $installerPath
-"Pythonインストール完了"
 
-# ChromeDriverのURL
-$chromeDriverUrl = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/win64/chromedriver-win64.zip"
-Invoke-WebRequest -Uri $chromeDriverUrl -OutFile "$env:TEMP\chromedriver.zip"
-Expand-Archive -Path "$env:TEMP\chromedriver.zip" -DestinationPath "C:\hogehoge" 
-Remove-Item -Path "$env:TEMP\chromedriver.zip" -Force
-"ChromeDriverインストール完了"
+param(
+    [switch]$a,
+    [switch]$p,
+    [switch]$d
+)
+
+function install_python {
+    # PythonのインストーラーのURL
+    $pythonUrl = "https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe" 
+    # Pythonのインストーラーのパス
+    $installerPath = "$env:TEMP\python_installer.exe"
+    Invoke-WebRequest -Uri $pythonUrl -OutFile $installerPath
+    Start-Process $installerPath -ArgumentList '/quiet InstallAllUsers=0 PrependPath=1' -Wait
+    Remove-Item $installerPath
+    Write-Output "Pythonインストール完了"
+}
+
+function install_webdriver {
+    # ChromeDriverのURL
+    $chromeDriverUrl = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/win64/chromedriver-win64.zip"
+    Invoke-WebRequest -Uri $chromeDriverUrl -OutFile "$env:TEMP\chromedriver.zip"
+    Expand-Archive -Path "$env:TEMP\chromedriver.zip" -DestinationPath "C:\hogehoge" 
+    Remove-Item -Path "$env:TEMP\chromedriver.zip" -Force
+    Write-Output "ChromeDriverインストール完了"
+}
+
+if ($a) {
+    install_python
+    install_webdriver
+} elseif ($p) {
+    install_python
+} elseif ($d) {
+    install_webdriver
+}
+
