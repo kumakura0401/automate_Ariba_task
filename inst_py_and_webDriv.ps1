eb8b87240f98c56b1eddafc9aa92ac8ad0d9e5b5
KUMAKURA <kumakura@lifematics.co.jp>
2024-01-30 01:48:08 +0900

    modified inst_py_and_webDriv.ps1

--- inst_py_and_webDriv.ps1
+++ inst_py_and_webDriv.ps1
@@ -3,40 +3,26 @@ param(
     [switch]$a,
     [switch]$p,
     [switch]$d
-)
+    )
 
 function install_python {
-    # PythonのインストーラーのURL
-    $pythonUrl = "https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe" 
-    # Pythonのインストーラーのパス
+    # PythonインストーラーのURL
+    $pythonUrl = "https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe"
+    # Pythonインストーラーのパス
     $installerPath = "$env:TEMP\python_installer.exe"
     Invoke-WebRequest -Uri $pythonUrl -OutFile $installerPath
     Start-Process $installerPath -ArgumentList '/quiet InstallAllUsers=0 PrependPath=1' -Wait
     Remove-Item $installerPath
-    Write-Output "Pythonインストール完了"
 }
 
 function install_webdriver {
     # ChromeDriverのURL
     $chromeDriverUrl = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/win64/chromedriver-win64.zip"
     Invoke-WebRequest -Uri $chromeDriverUrl -OutFile "$env:TEMP\chromedriver.zip"
-    Expand-Archive -Path "$env:TEMP\chromedriver.zip" -DestinationPath "C:\hogehoge" 
+    Expand-Archive -Path "$env:TEMP\chromedriver.zip" -DestinationPath "C:\hogehoge"
     Remove-Item -Path "$env:TEMP\chromedriver.zip" -Force
-    Write-Output "ChromeDriverインストール完了"
 }
 
-if ($a) {
-    install_python
-    install_webdriver
-} elseif ($p) {
-    install_python
-} elseif ($d) {
-    install_webdriver
-}
-
-
-#下記だと失敗する
-<#
 if ($a) {
     install_python
     install_webdriver
@@ -49,4 +35,5 @@ if ($p) {
 if ($d) {
     install_webdriver
 }
-#>
+
+
