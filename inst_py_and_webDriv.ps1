fb242ef10fb6667a5b919f1bd8c135e622c87413
Kumakura <LVF00047@astellas.com>
2024-01-29 10:59:33 +0900

    add commnt to inst file

--- inst_py_and_webDriv.ps1
+++ inst_py_and_webDriv.ps1
@@ -1,7 +1,15 @@
-
+# PythonのインストーラーのURL
+$pythonUrl = "https://www.python.org/ftp/python/3.10.4/python-3.10.4-amd64.exe" 
+# Pythonのインストーラーのパス
+$installerPath = "$env:TEMP\python_installer.exe"
+Invoke-WebRequest -Uri $pythonUrl -OutFile $installerPath
+Start-Process $installerPath -ArgumentList '/quiet InstallAllUsers=0 PrependPath=1' -Wait
+Remove-Item $installerPath
+"Pythonインストール完了"
 
 # ChromeDriverのURL
 $chromeDriverUrl = "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/121.0.6167.85/win64/chromedriver-win64.zip"
 Invoke-WebRequest -Uri $chromeDriverUrl -OutFile "$env:TEMP\chromedriver.zip"
-Expand-Archive -Path "$env:TEMP\chromedriver.zip" -DestinationPath "C:\hogehoge"  
-
+Expand-Archive -Path "$env:TEMP\chromedriver.zip" -DestinationPath "C:\hogehoge" 
+Remove-Item -Path "$env:TEMP\chromedriver.zip" -Force
+"ChromeDriverインストール完了"
